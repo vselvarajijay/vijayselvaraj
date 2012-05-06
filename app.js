03e8b26c964baeb7e4ea792bf26be8336670a8b0
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2012-05-06 06:00:50 -0400

    Added a video chat feature

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/test', routes.index);
+app.get('/chat', routes.videochat);
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Vijay Selvaraj' })
 };
+
+exports.videochat = function(req, res){
+  res.render('videochat', { title: 'Video Chat'})
+};
